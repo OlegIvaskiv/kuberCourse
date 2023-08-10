0643c376666d9a820b73d6d44dac044ed831e412
Oleh Ivaskiv <olegivaskiv1@gmail.com>
2023-08-10 10:57:57 +0300

    added react-app

---
+++ reactjs-demo/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (<div className="App">
+        <div>
+            <p>Name : Oleh</p>
+            <p>email : olegivaskiv1@.gmail.com</p>
+        </div>
+    </div>);
+}
+
+export default App;
