7e5847c7afd5b1f5b5980567d00e282f5a9e616a
Oleh Ivaskiv <olegivaskiv1@gmail.com>
2023-08-11 15:50:24 +0300

    added ui app

---
+++ bookmarker-ui-nextjs/pages/bookmarks/index.tsx
@@ -0,0 +1,34 @@
+import {GetServerSideProps, NextPage} from 'next'
+import {BookmarksResponse} from "@/services/model";
+import {fetchBookmarks} from "@/services/api";
+import Bookmarks from "@/components/Bookmarks";
+import SearchForm from "@/components/SearchForm";
+
+interface HomeProps {
+    bookmarks: BookmarksResponse
+    query?: string
+}
+
+
+const Home: NextPage<HomeProps> = (props) => {
+    return (
+        <div>
+            <SearchForm/>
+            <Bookmarks bookmarks={props.bookmarks} query={props.query}/>
+        </div>
+    )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const {page = 1, query = ""} = context.query
+    const bookmarks = await fetchBookmarks(parseInt(String(page)), String(query))
+    return {
+        props: {
+            bookmarks,
+            query
+        }
+    }
+}
+
+
+export default Home
